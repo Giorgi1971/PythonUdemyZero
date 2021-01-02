863c9b2e3f4fc81cb7b87a46bdf7e2e530eac3a4
Giorgi <giooorgi7@gmail.com>
2021-01-02 12:37:57 +0400

    this is logic begining

--- MileStoneProjecr2/game_cards.py
+++ MileStoneProjecr2/game_cards.py
@@ -1,4 +1,36 @@
-from classes import Player, Card
+import classes
 
 
-player_one = Player('One')
+player_one = classes.Player('One')
+player_two = classes.Player('Two')
+
+new_deck = classes.Deck()
+new_deck.shuffle()
+
+for x in range(26):
+    player_one.add_cards(new_deck.deal_one())
+    player_two.add_cards(new_deck.deal_one())
+
+game_on = True
+
+round_num = 0
+
+while game_on:
+    round_num += 1
+    print(f'Round {round_num}')
+
+    if len(player_one.all_cards) == 0:
+        print('Player One Lost !!!')
+        game_on = False
+        break
+
+    if len(player_two.all_cards) == 0:
+        print('Player Two Lost !!!')
+        game_on = False
+        break
+
+    player_one_cards = []
+    player_one_cards.append(player_one.remove_one())
+
+    player_two_cards = []
+    player_two_cards.append(player_two.remove_one())
